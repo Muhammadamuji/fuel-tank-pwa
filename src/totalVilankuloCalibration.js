d53b93b85ab952f3453fe5c63875e0cb150b898d
Muhammad Amuji <MUHAMMADAMUJIDC@GMAIL.COM>
2026-05-14 12:05:31 +0200

    Added TOTAL calibration file

---
+++ src/totalVilankuloCalibration.js
@@ -0,0 +1,20 @@
+export const totalTank1Points = [
+  [0,0],
+  [496,3898],
+  [2342,25026],
+];
+
+export const totalTank2Points = [
+  [0,0],
+  [2328,24805],
+];
+
+export const totalTank3Points = [
+  [0,0],
+  [1802,13987],
+];
+
+export const totalTank4Points = [
+  [0,0],
+  [1800,13956],
+];
